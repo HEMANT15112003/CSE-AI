829432e9d20eb26596f9361dff254800ef88c51e
HEMANT15112003 <93367586+HEMANT15112003@users.noreply.github.com>
2023-09-08 07:20:04 +0530

    Update Waterjug.py

--- Waterjug.py
+++ Waterjug.py
@@ -1,57 +1,89 @@
 from collections import deque
 
-def water_jug_problem(jug1_capacity, jug2_capacity, target_liters):
-    visited = set()
-    queue = deque([(0, 0, [])])  # Initial state: both jugs empty
 
-    while queue:
-        jug1, jug2, actions = queue.popleft()
+def BFS(a, b, target):
+    m = {}
+    isSolvable = False
+    path = []
 
-        if jug1 == target_liters or jug2 == target_liters:
-            return actions
-        
-        if (jug1, jug2) in visited:
+    q = deque()
+
+    q.append((0, 0))
+
+    while len(q) > 0:
+        u = q.popleft()  # If this state is already visited
+        if (u[0], u[1]) in m:
+            continue
+        if u[0] > a or u[1] > b or u[0] < 0 or u[1] < 0:
             continue
 
-        visited.add((jug1, jug2))
+        # Filling the vector for constructing
+        # the solution path
+        path.append([u[0], u[1]])
 
-        # Fill jug 1
-        if jug1 < jug1_capacity:
-            queue.append((jug1_capacity, jug2, actions + ['Fill Jug 1']))
+        # Marking current state as visited
+        m[(u[0], u[1])] = 1
 
-        # Fill jug 2
-        if jug2 < jug2_capacity:
-            queue.append((jug1, jug2_capacity, actions + ['Fill Jug 2']))
+        # If we reach the solution state, put ans=1
+        if u[0] == target or u[1] == target:
+            isSolvable = True
 
-        # Empty jug 1
-        if jug1 > 0:
-            queue.append((0, jug2, actions + ['Empty Jug 1']))
+            if u[0] == target:
+                if u[1] != 0:
+                    # Fill final state
+                    path.append([u[0], 0])
+            else:
+                if u[0] != 0:
+                    # Fill final state
+                    path.append([0, u[1]])
 
-        # Empty jug 2
-        if jug2 > 0:
-            queue.append((jug1, 0, actions + ['Empty Jug 2']))
+            # Print the solution path
+            sz = len(path)
+            for i in range(sz):
+                print("(", path[i][0], ",", path[i][1], ")")
+            break
 
-        # Pour from jug 1 to jug 2
-        pour_amount = min(jug1, jug2_capacity - jug2)
-        if pour_amount > 0:
-            queue.append((jug1 - pour_amount, jug2 + pour_amount, actions + ['Pour Jug 1 -> Jug 2']))
+        # If we have not reached the final state
+        # then, start developing intermediate
+        # states to reach the solution state
 
-        # Pour from jug 2 to jug 1
-        pour_amount = min(jug2, jug1_capacity - jug1)
-        if pour_amount > 0:
-            queue.append((jug1 + pour_amount, jug2 - pour_amount, actions + ['Pour Jug 2 -> Jug 1']))
+        # Fill Jug2
+        if u[1] < b:
+            q.append([u[0], b])
 
-    return None  # No solution found
+        # Fill Jug1
+        if u[0] < a:
+            q.append([a, u[1]])
 
-# Example usage
-jug1_capacity = int(input("Enter the capacity of jug 1: "))
-jug2_capacity = int(input("Enter the capacity of jug 2: "))
-target_liters = int(input("Enter the target amount to achieve: "))
-solution = water_jug_problem(jug1_capacity, jug2_capacity, target_liters)
+        # Pour water from Jug1 to Jug2
+        if u[0] > 0 and u[1] < b:
+            pour = min(u[0], b - u[1])
+            q.append([u[0] - pour, u[1] + pour])
 
-if solution:
-    # print(solution)
-    for step, action in enumerate(solution):
-        print(f"Step {step + 1}: {action}")
-else:
-    print("No solution found.")
+        # Pour water from Jug2 to Jug1
+        if u[1] > 0 and u[0] < a:
+            pour = min(u[1], a - u[0])
+            q.append([u[0] + pour, u[1] - pour])
+
+        # Empty Jug2
+        if u[1] > 0:
+            q.append([u[0], 0])
+
+        # Empty Jug1
+        if u[0] > 0:
+            q.append([0, u[1]])
+
+    # No solution exists if ans=0
+    if not isSolvable:
+        print("No solution")
+
+
+# Driver code
+if __name__ == '__main__':
+    Jug1 = int(input("Enter the capacity of Jug1: "))
+    Jug2 = int(input("Enter the capacity of Jug2: "))
+    target = int(input("Enter the target amount: "))
+    print("Path from initial state "
+          "to solution state ::")
+
+    BFS(Jug1, Jug2, target)
